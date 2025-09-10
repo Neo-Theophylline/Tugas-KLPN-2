ffab67489034633dc683e1598984c571098a1804
Archon <Theophyllineneo@gmail.com>
2025-09-10 14:49:47 +0700

    Kontroleer

--- Project-GAKKOU/routes/web.php
+++ Project-GAKKOU/routes/web.php
@@ -1,23 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeFrontendController;
+use App\Http\Controllers\Frontend\AboutFrontendController;
+use App\Http\Controllers\Frontend\ContactFrontendController;
+use App\Http\Controllers\Frontend\ServicesFrontendController;
+use App\Http\Controllers\Frontend\TestimonialsFrontendController;
 
-Route::get('/', function () {
-    return view('page.frontend.home.index');
-});
-
-Route::get('testimonials', function () {
-    return view('page.frontend.testimonials.index');
-});
-
-Route::get('about', function () {
-    return view('page.frontend.about.index');
-});
-
-Route::get('services', function () {
-    return view('page.frontend.services.index');
-});
-
-Route::get('contact', function () {
-    return view('page.frontend.contact.index');
-});
+Route::get('',[HomeFrontendController::class ,'index'] )->name('home');
+Route::get('about',[AboutFrontendController::class ,'index'] );
+Route::get('services',[ServicesFrontendController::class ,'index'] );
+Route::get('testimonials',[TestimonialsFrontendController::class ,'index'] );
+Route::get('contact',[ContactFrontendController::class ,'index'] );
